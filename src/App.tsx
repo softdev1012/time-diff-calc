6ea78e0a52557668f24271244fc0cd4312e845f3
softdev1012 <matthewjin1012@gmail.com>
2024-04-30 11:59:09 +0200

    init project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import Timezone from './Timezone';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Timezone />
+    </div>
+  );
+}
+
+export default App;
